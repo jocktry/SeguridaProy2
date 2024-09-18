d74bfcf323830b99e5b9a10b40413f7b94902413
Sebastian Choachi <juansebastianchv@hotmail.com>
2024-09-18 13:30:35 -0500

    formulario login y register

--- proyecto-seguridad-2/resources/views/auth/register.blade.php
+++ proyecto-seguridad-2/resources/views/auth/register.blade.php
@@ -1,77 +1,55 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+@extends("layouts.default")
+@section("title", "register")
+@section("content")
+<main class="mt-5">
+    <div class="container">
+        <div class="row justify-content-center">
+        <div class="col-md-4">
+            @if(session() -> has("success"))
+            <div class="alert alert-success">
+                {{session()->get("success")}}
+            </div>
+            @endif
+            @if(session() -> has("error"))
+            <div class="alert alert-danger">
+                {{session()->get("error")}}
+            </div>
+            @endif
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
-
+                <h3 class="card-header text-center">Register</h3>
                 <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
+                    <form method="POST" action='{{route("register.post")}}'>
                         @csrf
-
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group mb-3">
+                            <input type="text" placeholder="Fullname" id="fullname" class="form-control"
+                                name="fullname" autofocus>
+                        @if ($errors->has('fullname'))
+                            <span class="text-danger">{{ $errors->first('fullname')}}</span>
+                        @endif
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group mb-3">
+                            <input type="text" placeholder="Email" id="email" class="form-control"
+                                name="email" required autofocus>
+                        @if ($errors->has('email'))
+                            <span class="text-danger">{{ $errors->first('email')}}</span>
+                        @endif
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="form-group mb-3">
+                            <input type="password" placeholder="Password" id="password" class="form-control"
+                                name="password" required>
+                        @if ($errors->has('password'))
+                            <span class="text-danger">{{ $errors->first('password')}}</span>
+                        @endif
                         </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
+                        <div class="d-grid mx-auto">
+                            <button type="submit" class="btn btn-dark btn-block">Signup</button>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
+        </div>
     </div>
-</div>
+</main>
 @endsection
+
--- proyecto-seguridad-2/routes/web.php
+++ proyecto-seguridad-2/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware("auth")->group(function(){
+    Route::view('/', 'welcome') -> name("home");
 });
 
-Auth::routes();
+Route::get("/login", [AuthController::class, "login"]) -> name("login");
+Route::post("/login", [AuthController::class, "loginPost"]) -> name("login.post");
+
+Route::get("/register", [AuthController::class, "register"])-> name("register");
+Route::post("/register", [AuthController::class, "registerPost"]) -> name("register.post");
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
